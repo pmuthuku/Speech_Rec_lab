9c7b44b8701ca2886bc3402dc5558721ef7002c9
Prasanna Kumar Muthukumar <pmuthuku@cs.cmu.edu>
2014-02-02 11:50:06 -0500

    Added Endpoint detection to test_rec.py. Also added test_play.py and do_HW1.sh
    do_HW1.sh is a wrapper script for all the other scripts in the
    directory. test_rec.py now does naive endpoint detection using log
    energy. test_play.py is a vanilla pyaudio script that is used to
    replay the recorded audio.

---
+++ do_HW1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python test_rec.py
+
+# Converting audio to 16KHz mono
+ch_wave output.wav -c 0 -F 16000 -o right_output.wav
+rm -f output.wav
+
+# Replaying recorded audio
+python test_play.py right_output.wav
--- test_rec.py
+++ test_rec.py
@@ -1,12 +1,16 @@
 import pyaudio
 import wave
+import array
+import numpy
 
-CHUNK = 1024
+CHUNK = 1225
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 10
+RECORD_SECONDS = 20
 WAVE_OUTPUT_FILENAME = "output.wav"
+
+threshold = 200
 
 p = pyaudio.PyAudio()
 
@@ -20,15 +24,34 @@ print("* recording")
 
 frames = []
 
+num_sil_frames = 0
+
+# The device will record for 20 seconds until there has been 1
+# second of audio (36 frames) where the energy is less than the
+# threshold
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(data)
+    nums = array.array('h',data)
+    left = numpy.array(nums[1::2])
+    energy = 10*numpy.log(numpy.sum(numpy.power(left,2)))
+    #print(energy)
+
+    if (energy < threshold):
+        num_sil_frames = num_sil_frames + 1
+    else:
+        num_sil_frames = 0
+
+    if num_sil_frames >= 37:
+        break
 
 print("* done recording")
 
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
